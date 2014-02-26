53713f5c534d4a34b95cf2777689a795eaa09262
Arie <arie@wego.com>
2014-02-26 18:47:45 +0700

    Initial Commit

---
+++ Intan TV/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Intan TV
+//
+//  Created by Flash on 2/26/14.
+//  Copyright (c) 2014 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "netraAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([netraAppDelegate class]));
+	}
+}
